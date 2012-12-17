4a1335bcd27e82a4807f4d98cc74e2dc025c315a
Randall Crock <rlcrock@gmail.com>
2012-12-17 11:03:12 -0500

    Initial source upload
    Contains basic commands /lag, /drops, /loadedchunks, and /dimid.

---
+++ ForgeTools/src/forgetools/common/DimIDCommand.java
@@ -0,0 +1,27 @@
+package forgetools.common;
+
+import cpw.mods.fml.common.FMLCommonHandler;
+import net.minecraft.src.CommandBase;
+import net.minecraft.src.EntityPlayerMP;
+import net.minecraft.src.ICommandSender;
+
+public class DimIDCommand extends CommandBase
+{
+
+	@Override
+	public String getCommandName()
+	{
+		return "dimid";
+	}
+
+	@Override
+	public void processCommand(ICommandSender sender, String[] args)
+	{
+		if(!FMLCommonHandler.instance().getEffectiveSide().isServer()) return;
+		EntityPlayerMP player = getCommandSenderAsPlayer(sender);
+		
+		sender.sendChatToPlayer("\u00a72You are currently in " + player.worldObj.getWorldInfo().getWorldName() + " " +
+								player.worldObj.provider.getDimensionName() + " (dim id " + player.worldObj.provider.dimensionId + ")");
+	}
+
+}
---
+++ ForgeTools/src/forgetools/common/ForgeTools.java
@@ -0,0 +1,39 @@
+package forgetools.common;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.src.ServerCommandManager;
+import net.minecraftforge.common.MinecraftForge;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.ServerStarting;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+
+@Mod(modid="ForgeTools_0_1", name="Forge Tools", version="0.1")
+@NetworkMod(clientSideRequired=false, serverSideRequired=false)
+
+public class ForgeTools
+{
+	
+	public static MinecraftServer server;
+	
+	@Init
+	public void Init(FMLInitializationEvent event)
+	{
+	}
+	
+	@ServerStarting
+	public void serverStarting(FMLServerStartingEvent event)
+	{
+		server = FMLCommonHandler.instance().getMinecraftServerInstance();
+		ServerCommandManager manager = (ServerCommandManager)server.getCommandManager();
+		manager.registerCommand(new LagCommand());
+		manager.registerCommand(new DropsCommand());
+		manager.registerCommand(new LoadedChunksCommand());
+		manager.registerCommand(new DimIDCommand());
+		System.out.println("ForgeTools: Registered 4 commands");
+	}
+}
