e546b1eb00443ab6d231e55de6fb64d50f5e5415
asudol <asudol@gmail.com>
2012-12-21 11:58:31 -0500

    Reordered the arugments on the heal command

--- ForgeTools/src/forgetools/common/HealCommand.java
+++ ForgeTools/src/forgetools/common/HealCommand.java
@@ -22,7 +22,7 @@ public class HealCommand extends CommandBase{
 	@Override
 	public String getCommandUsage(ICommandSender par1ICommandSender)
     {
-    	return "/heal [hp | hunger | username] [amount] [username]";
+    	return "/heal [username] [hp | hunger] [amount]";
     }
 
 	@Override
@@ -40,64 +40,77 @@ public class HealCommand extends CommandBase{
 		}
 		
 		boolean full = false, hp = false, food = false;
+		int amt = 20;
 		
 		// Only accept the command with one or three arguments
-		if (args.length == 1)	full = true;
-		else if (args.length == 3) { 
-			if (args[0].equals("hp"))
+		if (args.length == 1)
+			full = true;
+		else if (args.length == 2) { 
+			if (args[1].equals("hp")) {
 				hp = true;
-			else if (args[0].equals("hunger"))
+				full = true;
+			}
+			else if (args[1].equals("hunger")) {
 				food = true;
+				full = true;
+			}
 			else
 				throw new WrongUsageException(getCommandUsage(sender));
 		}
-		else	throw new WrongUsageException(getCommandUsage(sender));
-		
-		String user = "";
-		int amt = 20;
-		if (full)
-			user = args[0];
-		else {
-			user = args[2];
-			double temp = Double.parseDouble(args[1]);
-			amt = (int) (temp * 2);
+		else if (args.length == 3) {
+			if (args[1].equals("hp"))
+				hp = true;
+			else if (args[1].equals("hunger"))
+				food = true;
+			else
+				throw new WrongUsageException(getCommandUsage(sender));
+			
+			double temp = 0;
+			try {
+				temp = Double.parseDouble(args[2]);
+				amt = Math.min((int) (temp * 2), 20);
+			} catch (NumberFormatException e) {
+				sender.sendChatToPlayer("\u00a7cPlease enter a valid number for the heal amount.");
+				return;
+			}
+			
+			if (amt < 0) {
+				sender.sendChatToPlayer("\u00a7cYou cannot heal for a negative amount.");
+				return;
+			}
 		}
-		
-		if (amt < 0) {
-			sender.sendChatToPlayer("\u00a7cYou cannot heal for a negative amount");
-			return;
-		}
-		
+		else 
+			throw new WrongUsageException(getCommandUsage(sender));
 		
 		String players[] = serverConfig.getAllUsernames();	// Get an array of all usernames
 		boolean found = false;
 		for (String s: players) {							// Search for the targeted username
-			if (s.equals(user))
+			if (s.equals(args[0]))
 				 found = true;
 		}
 		
 		if (found) {
-			EntityPlayerMP target = serverConfig.getPlayerForUsername(user);
+			EntityPlayerMP target = serverConfig.getPlayerForUsername(args[0]);
 			
-			if (full) {
-				sender.sendChatToPlayer("\u00a7aHealing " + user + "'s HP and hunger to full");
+			if (full && !hp && !food) {
+				sender.sendChatToPlayer("\u00a7aHealing " + args[0] + "'s HP and hunger to full.");
 				target.heal(20);
 				target.getFoodStats().addStats(20, 20);
-				target.sendChatToPlayer("\u00a7a" + player.username + " has healed your HP and hunger to full");
+				target.sendChatToPlayer("\u00a7a" + player.username + " has healed your HP and hunger to full.");
 			} else {
-				sender.sendChatToPlayer("\u00a7aHealing " + user + "'s " + ((hp == true) ? "hp" : "hunger") + " by " + amt / 2);
+				sender.sendChatToPlayer("\u00a7aHealing " + args[0] + "'s " + ((hp == true) ? "hp" : "hunger") + " by " + (double)(amt) / 2 + ".");
 				if (hp) {
 					target.heal(amt);
 					
-					target.sendChatToPlayer("\u00a7a" + player.username + " has healed your HP by " + amt / 2 + " hearts");
+					target.sendChatToPlayer("\u00a7a" + player.username + " has healed your HP by " + (double)(amt) / 2 + " hearts.");
 				} else {
 					target.getFoodStats().addStats(amt, 20);
 					
-					target.sendChatToPlayer("\u00a7a" + player.username + " has healed your hunger by " + amt / 2 + " drumsticks");
+					target.sendChatToPlayer("\u00a7a" + player.username + " has healed your hunger by " + (double)(amt) / 2 + " drumsticks.");
 				}
 			}
 		} else
-			sender.sendChatToPlayer("\u00a7c" + user + " cannot be found");
+			sender.sendChatToPlayer("\u00a7c" + args[0] + " cannot be found.");
 	}
 
 }
